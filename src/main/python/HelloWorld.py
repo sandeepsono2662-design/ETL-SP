4d611c5ff02cf5d881a129a4c0a4792065b06912
sandeepsono2662-design <sandeep.sono.2662@gmail.com>
2026-09-15 21:52:44 +0530

    new files

--- src/main/python/HelloWorld.py
+++ src/main/python/HelloWorld.py
@@ -1,13 +1,28 @@
-text = "hello world hello python world"
+from pyspark.sql import SparkSession
 
-words = text.split()
+# Create Spark Session
+spark = SparkSession.builder \
+    .appName("WordCount") \
+    .getOrCreate()
 
-word_count = {}
+# Create RDD
+data = [
+    "Hello world",
+    "PySpark is powerful",
+    "Hello PySpark"
+]
 
-for word in words:
-    if word in word_count:
-        word_count[word] += 1
-    else:
-        word_count[word] = 1
+rdd = spark.sparkContext.parallelize(data)
 
-print(word_count)
+# Word Count
+word_counts = (
+    rdd.flatMap(lambda line: line.split())
+       .map(lambda word: (word, 1))
+       .reduceByKey(lambda a, b: a + b)
+)
+
+# Display results
+for word, count in word_counts.collect():
+    print(word, count)
+
+spark.stop()
